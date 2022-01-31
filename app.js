d18a512a423fc862534e7a06bc67caa7bf4f7d88
Eunjin Jeoung <mayjung0925@gmail.com>
2022-01-31 14:48:56 -0600

    const instead of  let & for loop to show interests in order

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 
-
-let resume = {
+const resume = {
     name :  'Eunjin Jeong',
     career : 'Web Developer',
     description : 'I believe consistency wins the race.'
@@ -9,21 +8,18 @@ console.log('Name:'+resume.name.toUpperCase());
 console.log('Career:'+resume.career);
 console.log('Description:'+resume.description);
 
-// Is there a better way to add a line break in console log?
-let linebreak1 ='\n'
-console.log(linebreak1);
+console.log('');
+
 
 console.log('My Interests : ');
-interests ('camping');
-interests('knitting');
-interests('coding');
 
-let linebreak2 ='\n'
-console.log(linebreak2);
-
-function interests(text){
-    console.log('*'+text);
+const myInterests = ['camping','knitting','coding']
+for(let i=0 ; i <myInterests.length ; i++){
+    console.log('* '+myInterests[i])
 }
+
+console.log('');
+
 
 
 
@@ -35,12 +31,10 @@ displayPosition('Korens Inc.', 'Inspector', 'Examined  materials and products fo
 
 
 function displayPosition(company, job, description) {
- console.log('*' + company + ':' + job+ '-' + description);
+ console.log('* ' + company + ' : ' + job+ ' - ' + description);
 
 }
-
-let linebreak3 ='\n'
-console.log(linebreak3);
+console.log('');
 
 console.log('My Skills :');
 
@@ -52,13 +46,17 @@ displaySkill('HTML & CSS',false );
 
 function displaySkill(skill,cool){
 
-  
   if(cool === true){
-      console.log('*BAM :'+ ' '+skill)
+      console.log('*BAM : '+skill)
 
   }else{
-      console.log('*'+skill);
+      console.log('* '+skill);
   }
 
 }
+
+
+
+
+
 
